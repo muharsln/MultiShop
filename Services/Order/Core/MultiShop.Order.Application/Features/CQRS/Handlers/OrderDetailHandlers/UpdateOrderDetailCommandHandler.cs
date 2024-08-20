cd7713e08922fdcc329f39ed71bef77f2906a9de
Muhammed ARSLAN <muhammedg3@gmail.com>
2024-08-20 11:24:55 +0300

    feat(Order): Basic crud processing was done with CQRS.

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/UpdateOrderDetailCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
+public class UpdateOrderDetailCommandHandler
+{
+    private readonly IRepository<OrderDetail> _repository;
+
+    public UpdateOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task Handle(UpdateOrderDetailCommand command)
+    {
+        var value = await _repository.GetByIdAsync(command.OrderDetailId);
+        value.OrderingId = command.OrderingId;
+        value.ProductId = command.ProductId;
+        value.ProductName = command.ProductName;
+        value.ProductAmount = command.ProductAmount;
+        value.ProductPrice = command.ProductPrice;
+        value.ProductTotalPrice = command.ProductTotalPrice;
+        await _repository.UpdateAsync(value);
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Queries/OrderDetailQueries/GetOrderDetailByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+
+public class GetOrderDetailByIdQuery(int id)
+{
+    public int Id { get; set; } = id;
+}
